c98a98b0537898d0a6f9354b44dbb2522b927f06
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2014-12-20 23:17:35 +0530

    Marking all read/unread

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function(){
 		return this.store.find('mail-thread');
+	},
+	_updateMailReadStatus: function(readStatus) {
+		this.currentModel.forEach(function(mailThread){
+				mailThread.get('mails').forEach(function(mail){
+					mail.set('isMailRead', readStatus);
+				})
+			});
+	},
+	actions: {
+		markAllRead: function(){
+			this._updateMailReadStatus(true);
+		},
+		markAllUnread: function(){
+			this._updateMailReadStatus(false);
+		}
 	}
 });
